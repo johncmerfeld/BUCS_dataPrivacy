4a8fbd39783ca46ca013d31118a263dfbca7241a
John Merfeld <john.merfeld@gmail.com>
2018-11-08 09:33:46 -0500

    working on cats vs dogs

---
+++ catVsDog.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
--- tensorflowPractice.py
+++ tensorflowPractice.py
@@ -4,6 +4,7 @@ from tensorflow import keras
 
 # Helper libraries
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 
 # possible warning suppressing stuff
@@ -97,3 +98,93 @@ for i in range(num_images):
   plt.subplot(num_rows, 2*num_cols, 2*i+2)
   plot_value_array(i, predictions, test_labels)
   
+  
+  
+img = cv2.imread("img.jpg", 0)
+img = img / 256
+
+test_images[0] = img
+predictions = model.predict(test_images)
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  #thisplot[true_label].set_color('blue')
+
+i = 1
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  test_labels)
+
+img2 = cv2.imread("img2.jpg", 0)
+img2 = img2 / 256
+
+test_images[1] = img2
+  
+
+img3 = cv2.imread("img3.png", 0)
+img3 = img3 / 256
+
+test_images[2] = img3
+
+
+i = 2
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  test_labels)
+
+
+img4 = cv2.imread("img4.jpg", 0)
+img4 = img4 / 256
+
+test_images[3] = img4
+
+
+i = 3
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  test_labels)
+
+
+new_images = test_images[0:3]
+
+
+img4 = cv2.imread("img6.jpg", 0)
+img4 = img4 / 256
+
+new_images[2] = img4
+
+
+new_preds = model.predict(new_images)
