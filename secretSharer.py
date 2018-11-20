da3b3bb451329b48df3282c1f6221b60fa2d6bb6
John Merfeld <john.merfeld@gmail.com>
2018-11-20 00:33:21 -0500

    lots of string cleaning

--- secretSharer.py
+++ secretSharer.py
@@ -30,13 +30,23 @@ def cleanSMS(sms):
     # leetspeak
     
     sms = re.sub(" 2 ", " to ", sms)
-    sms = re.sub(" 4 ", " for ", sms)
+    sms = re.sub(" 4 | fr ", " for ", sms)
     
     sms = re.sub(" abt ", " about ", sms)
     sms = re.sub(" aft ", " after ", sms)
+    sms = re.sub(" ard ", " around ", sms)
+    
+    sms = re.sub(" ar ", " all right ", sms)
+    sms = re.sub(" ar$", " all right", sms)
+    
     sms = re.sub(" b ", " be ", sms)
+    
     sms = re.sub(" c ", " see ", sms)
+    sms = re.sub("^c ", "see ", sms)
+    
     sms = re.sub(" coz | cuz | cos ", " cause ", sms)
+    sms = re.sub("^coz |^cuz |^cos ", "cause ", sms)
+    
     sms = re.sub(" da ", " the ", sms)
     sms = re.sub(" dat ", " that ", sms)
     sms = re.sub(" din ", " didnt ", sms)
@@ -46,18 +56,31 @@ def cleanSMS(sms):
     sms = re.sub("^dun ", "dont ", sms)
     sms = re.sub(" dun$", " dont", sms)
     
+    sms = re.sub(" frens ", " friends ", sms)
+    
     sms = re.sub(" gd ", " good ", sms)
     sms = re.sub("^gd ", "good ", sms)
     sms = re.sub(" gd$", " good", sms)
     
     sms = re.sub(" haf ", " have ", sms)
-    sms = re.sub(" juz ", " just ", sms)
+    sms = re.sub(" haf$", " have", sms)
     
+    sms = re.sub(" juz ", " just ", sms)
+    sms = re.sub("^juz ", "just ", sms)
+    
+    sms = re.sub(" lar ", " later ", sms)
+    sms = re.sub(" lar$", " later", sms)
+    sms = re.sub("^lar ", "later ", sms)
+    
+    sms = re.sub(" lect ", " lecture ", sms)
+    sms = re.sub("^ll ", "ill ", sms)
     sms = re.sub("^m ", "im ", sms)
     sms = re.sub(" mayb ", " maybe ", sms)
     sms = re.sub(" meh ", " me ", sms)
+    sms = re.sub(" neva ", " never ", sms)
     sms = re.sub(" muz ", " must ", sms)
     sms = re.sub(" n ", " and ", sms)
+    sms = re.sub(" nite ", " night ", sms)
     sms = re.sub(" noe ", " know ", sms)
     
     sms = re.sub(" okie | ok | k ", " okay ", sms)
@@ -75,23 +98,26 @@ def cleanSMS(sms):
     sms = re.sub(" r$", " are", sms)
     
     sms = re.sub(" rem ", " remember ", sms)
+    sms = re.sub(" rite ", " right ", sms)
     sms = re.sub("^s ", "its ", sms)
     
     sms = re.sub(" sch ", " school ", sms)
     sms = re.sub(" sch$", " school", sms)
     
     sms = re.sub(" tmr ", " tomorrow ", sms)
+    sms = re.sub("^tmr ", "tomorrow ", sms)
     
     sms = re.sub(" thanx ", " thanks ", sms)
     sms = re.sub(" thanx$", " thanks", sms)
     sms = re.sub(" thk ", " think ", sms)
     sms = re.sub(" tot " , " thought ", sms)
     
-    sms = re.sub(" [uü] ", " you ", sms)
-    sms = re.sub("^[uü] ", "you ", sms)
-    sms = re.sub(" [uü]$", " you", sms)
+    sms = re.sub(" [uüü] ", " you ", sms)
+    sms = re.sub("^[uüü] ", "you ", sms)
+    sms = re.sub(" [uüü]$", " you", sms)
     
     sms = re.sub(" ur ", " your ", sms)
+    sms = re.sub(" v ", " very ", sms)
     sms = re.sub(" wan ", " want ", sms)
     
     sms = re.sub(" wat ", " what ", sms)
@@ -99,21 +125,34 @@ def cleanSMS(sms):
     sms = re.sub(" wat$", " what", sms)
     
     sms = re.sub(" wif ", " with ", sms)
+    sms = re.sub(" wun ", " wont ", sms)
     
     sms = re.sub(" y ", " why ", sms)
     sms = re.sub("^y ", "why ", sms)
     sms = re.sub(" y$", " why", sms)
 
     # remove laughter
-    sms = re.sub(" ha ", "", sms)
+    sms = re.sub(" ha ", " ", sms)
+    sms = re.sub(" lor ", " ", sms)
     sms = re.sub("a*(ha){2,}h*", "", sms)
     
+    # remove words I don't understand
+    sms = re.sub(" lei ", " ", sms)
+    sms = re.sub("^lei ", " ", sms)
+    sms = re.sub(" lei$", " ", sms)
+    
+    # standardize '-ing' to '-in'
+    sms = re.sub("(?<=[bdfghklmnoprstvwy])ing(?= )", "in", sms)
+    sms = re.sub("(?<=[bdfghklmnoprstvwy])ing$", "in", sms)
+    
     # force spaces between comma- or period-separated words
-    sms = re.sub("(?<=[^ ])[\.,](?<=[^ ])", " ", sms)
+    sms = re.sub("(?<=[^ ])[\.,](?=[^ ])", " ", sms)
     
     return sms
     
 dataRaw['splchk'] = dataRaw['noPunc'].apply(cleanSMS)
+dataRaw['splchk'] = dataRaw['splchk'].apply(cleanSMS)
+
 
 dataRaw
 
